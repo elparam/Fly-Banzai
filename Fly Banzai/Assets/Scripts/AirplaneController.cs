25df021086cf8079ce63b307ca2d2002d301d1d8
Eldar <timekillerg@gmail.com>
2015-03-01 02:19:01 +0300

    Add Unity Project, First dev checkin

---
+++ Fly Banzai/Assets/Scripts/AirplaneController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class AirplaneController : MonoBehaviour
+{
+    private bool _isFly = false;
+    Quaternion FlyUpRotaion = new Quaternion(0f,0f,0.1f,0f);
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        FlyUp();
+    }
+
+    private void FlyUp()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _isFly = true;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            _isFly = false;
+        }
+        if (_isFly)
+        {
+            RotateAirplaneBody();
+            gameObject.rigidbody2D.AddForce(Vector2.up*Time.deltaTime*50);
+        }
+    }
+
+    private void RotateAirplaneBody()
+    {
+        //transform.rotation = Quaternion.Lerp(transform.rotation, FlyUpRotaion, Time.deltaTime*10);
+    }
+}
---
+++ Fly Banzai/Assets/Scripts/PropellerRotator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PropellerRotator : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+        transform.Rotate(new Vector3(0,0, -1) * Time.deltaTime * 1000);
+	}
+}
